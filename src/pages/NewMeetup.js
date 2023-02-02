429a5b54eda1d68e1d4e3df00cc70cd2f5f94ca9
chigozirim1999 <ndinechilex@gmail.com>
2023-02-02 14:31:45 +0100

    useNavigate replaced useHistory

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
+// import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    // const history = useHistory();
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch("https://react-todo-app-b2070-default-rtdb.firebaseio.com/meetups.json", {
+            method: "POST",
+            body: JSON.stringify(meetupData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then(() => {
+            navigate("/");
+        });
+    }
+
     return (
         <section>
             <h1>New Meetup Page</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
